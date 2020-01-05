68024aeb1dd4c07c4f0c36609eec136cf26e3960
Your Name <samanbatool08@gmail.com>
2020-01-05 18:09:48 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Write your code here
+
+const var1 = 31
+const var2 = 2
+
+const multiply = var1 * var2;
+
+const random = Math.floor(Math.random() * 10) + 1;
+
+const mod = 16 % 6;
+
+const max = Math.max(1, 2, 20);
